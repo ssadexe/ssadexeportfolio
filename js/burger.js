1ab646ed5ae122fc2c51579c7d14e87c30517996
ssadexe <diomin.vd13@icloud.com>
2022-02-15 23:01:10 +0300

    add services block, redisign site, new images

--- js/burger.js
+++ js/burger.js
@@ -4,11 +4,11 @@ let closeBurgerBtn = document.getElementById('close-burger-btn');
 burgerBtn.onclick = function openMenu() {
     document.getElementById('burger-menu').classList.toggle('burger-vis');
     document.getElementById('burger-btn').classList.toggle('burger-btn-anim');
-    document.getElementById('main-img-box').classList.toggle('main-img-box-hide');
+    
 }
 
 closeBurgerBtn.onclick = function closeMenu() {
     document.getElementById('burger-menu').classList.toggle('burger-vis');
     document.getElementById('burger-btn').classList.toggle('burger-btn-anim');
-    document.getElementById('main-img-box').classList.toggle('main-img-box-hide');
+    
 }
--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -1,1 +1,2 @@
-//ScrollReveal().reveal('.content-box', { delay: 500 });
+ScrollReveal().reveal('.person', { delay: 300 });
+ScrollReveal().reveal('.card', { delay: 300 });
