298c39a6a344ead95d69deb6beefd102660bb3d8
ssadexe <diomin.vd13@icloud.com>
2022-02-15 23:28:07 +0300

    edit

--- js/burger.js
+++ js/burger.js
@@ -4,11 +4,9 @@ let closeBurgerBtn = document.getElementById('close-burger-btn');
 burgerBtn.onclick = function openMenu() {
     document.getElementById('burger-menu').classList.toggle('burger-vis');
     document.getElementById('burger-btn').classList.toggle('burger-btn-anim');
-    
 }
 
 closeBurgerBtn.onclick = function closeMenu() {
     document.getElementById('burger-menu').classList.toggle('burger-vis');
     document.getElementById('burger-btn').classList.toggle('burger-btn-anim');
-    
 }
